7c37e278bc71e679eba248182301cf49bcea16a1
Johannes Hengstler <ubezl@student.kit.edu>
2023-05-07 00:49:38 +0200

    added constructor and other collection functions

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,16 @@ pub struct BitVector {
 }
 
 impl BitVector {
+    /// Create a new empty bitvector.
+    pub fn new() -> BitVector {
+        BitVector {
+            data: Vec::new(),
+            len: 0,
+            blocks: vec![BlockDescriptor { zeros: 0 }],
+            super_blocks: vec![BlockDescriptor { zeros: 0 }],
+        }
+    }
+
     /// Appends a new 0 to the end of `data`. If the last block is full, a new block is
     /// created. If the last super block is full, a new super block is created.
     fn begin_new_word(&mut self) {
@@ -63,54 +73,101 @@ impl BitVector {
         }
     }
 
+    /// Appends a word to the end of the vector. The word is interpreted as a sequence of
+    /// bits, with the least significant bit being the first one (i.e. the word is
+    /// interpreted as little-endian). The vector will be extended by the number of bits
+    /// in the word.
+    ///
+    /// It is a logic error if the word has a partial word at its end before this operation
+    /// (i.e. the length of the vector must be a multiple of the word size).
+    pub fn append_word(&mut self, word: u64) {
+        debug_assert!(self.len % WORD_SIZE == 0);
+
+        self.begin_new_word();
+        *self.data.last_mut().unwrap() = word;
+        self.len += WORD_SIZE;
+
+        self.blocks.last_mut().unwrap().zeros += WORD_SIZE - word.count_ones() as usize;
+        self.super_blocks.last_mut().unwrap().zeros += WORD_SIZE - word.count_ones() as usize;
+    }
+
+    /// Return the 0-rank of the bit at the given position. The 0-rank is the number of
+    /// 0-bits in the vector up to but excluding the bit at the given position.
+    ///
+    /// # Parameters
+    /// - `pos`: The position of the bit to return the rank of.
     pub fn rank0(&self, pos: usize) -> usize {
         self.rank(true, pos)
     }
 
+    /// Return the 1-rank of the bit at the given position. The 1-rank is the number of
+    /// 1-bits in the vector up to but excluding the bit at the given position.
+    ///
+    /// # Parameters
+    /// - `pos`: The position of the bit to return the rank of.
     pub fn rank1(&self, pos: usize) -> usize {
         self.rank(false, pos)
     }
 
-    pub fn rank(&self, zero: bool, pos: usize) -> usize {
+    // I measured 5-10% improvement with this. I don't know why it's not inlined by default, the
+    // branch elimination profits alone should make it worth it.
+    #[inline(always)]
+    fn rank(&self, zero: bool, pos: usize) -> usize {
         let index = pos / WORD_SIZE;
         let block_index = pos / BLOCK_SIZE;
         let super_block_index = pos / SUPER_BLOCK_SIZE;
         let mut rank = 0;
 
         if super_block_index > 0 {
-            if zero {
-                rank += self.super_blocks[super_block_index - 1].zeros;
+            rank += if zero {
+                self.super_blocks[super_block_index - 1].zeros
             } else {
-                rank += (super_block_index * SUPER_BLOCK_SIZE)
-                    - self.super_blocks[super_block_index - 1].zeros;
-            }
+                (super_block_index * SUPER_BLOCK_SIZE)
+                    - self.super_blocks[super_block_index - 1].zeros
+            };
         }
 
         if block_index > 0 {
-            if zero {
-                rank += self.blocks[block_index - 1].zeros;
+            rank += if zero {
+                self.blocks[block_index - 1].zeros
             } else {
-                rank += (block_index * BLOCK_SIZE) - self.blocks[block_index - 1].zeros;
-            }
+                (block_index * BLOCK_SIZE) - self.blocks[block_index - 1].zeros
+            };
         }
 
         for i in
             ((super_block_index * SUPER_BLOCK_SIZE) + (block_index * BLOCK_SIZE)) / WORD_SIZE..index
         {
-            if zero {
-                rank += self.data[i].count_zeros() as usize;
+            rank += if zero {
+                self.data[i].count_zeros() as usize
             } else {
-                rank += self.data[i].count_ones() as usize;
-            }
+                self.data[i].count_ones() as usize
+            };
         }
 
-        if zero {
-            rank += (!self.data[index] & ((1 << (pos % WORD_SIZE)) - 1)).count_ones() as usize;
+        rank += if zero {
+            (!self.data[index] & ((1 << (pos % WORD_SIZE)) - 1)).count_ones() as usize
         } else {
-            rank += (self.data[index] & ((1 << (pos % WORD_SIZE)) - 1)).count_ones() as usize;
-        }
+            (self.data[index] & ((1 << (pos % WORD_SIZE)) - 1)).count_ones() as usize
+        };
 
         rank
+    }
+
+    /// Return the length of the vector, i.e. the number of bits it contains.
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    /// Return whether the vector is empty.
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+}
+
+impl Default for BitVector {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
