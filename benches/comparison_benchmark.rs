dd2bb36bde5e52c8cfb45cb253c9cb1bda9bbb70
Johannes Hengstler <ubezl@student.kit.edu>
2023-05-07 21:03:29 +0200

    wrong vector construction caused way too long vector

--- benches/comparison_benchmark.rs
+++ benches/comparison_benchmark.rs
@@ -9,7 +9,7 @@ fn construct_vers_vec(rng: &mut ThreadRng, len: usize) -> BitVector {
     let sample = Uniform::new(0, u64::MAX);
 
     let mut bit_vec = BitVector::new();
-    for _ in 0..len {
+    for _ in 0..len / 64 {
         bit_vec.append_word(sample.sample(rng));
     }
 
@@ -18,7 +18,7 @@ fn construct_vers_vec(rng: &mut ThreadRng, len: usize) -> BitVector {
 
 fn construct_rsdict_vec(rng: &mut ThreadRng, len: usize) -> RsDict {
     let mut rs_dict = RsDict::with_capacity(len * 64);
-    for _ in 0..len * 64 {
+    for _ in 0..len {
         rs_dict.push(rng.gen_bool(0.5));
     }
     rs_dict
@@ -34,7 +34,7 @@ fn compare_ranks(b: &mut Criterion) {
         let rsdict = construct_rsdict_vec(&mut rng, l);
         let sample = Uniform::new(0, l);
 
-        group.bench_with_input(BenchmarkId::new("vers", l), &l, |b, _| {
+        group.bench_with_input(BenchmarkId::new("vers.rank0", l), &l, |b, _| {
             b.iter_batched(
                 || sample.sample(&mut rng),
                 |e| black_box(vers_vec.rank0(e)),
