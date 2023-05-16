47bc86a2be71934e364bfcbfc4906c2d6e0afef5
Johannes Hengstler <ubezl@student.kit.edu>
2023-05-16 20:23:28 +0200

    strengthened select benchmark against overshooting random ranks

--- benches/select_benchmark.rs
+++ benches/select_benchmark.rs
@@ -10,7 +10,7 @@ fn bench_select(b: &mut Criterion) {
     let mut group = b.benchmark_group("select");
     for l in [2 << 8, 2 << 10, 2 << 12, 2 << 14, 2 << 16, 2 << 18, 2 << 20] {
         let bit_vec = common::construct_vers_vec(&mut rng, l);
-        let sample = Uniform::new(0, bit_vec.len() / 2 - 100);
+        let sample = Uniform::new(0, bit_vec.len() / 4);
         group.bench_with_input(format!("{} elements", l), &l, |b, _| {
             b.iter_batched(
                 || sample.sample(&mut rng),
