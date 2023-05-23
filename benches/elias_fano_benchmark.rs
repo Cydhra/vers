063705ad45b3354feb247585ead36ded0b63dcd9
Johannes Hengstler <ubezl@student.kit.edu>
2023-05-23 16:38:29 +0200

    sort sequence in ef-benchmark, so it doesn't violate the method contract

--- benches/elias_fano_benchmark.rs
+++ benches/elias_fano_benchmark.rs
@@ -8,12 +8,12 @@ fn bench_ef(b: &mut Criterion) {
 
     let mut group = b.benchmark_group("predecessor");
     for l in [2 << 8, 2 << 10, 2 << 12, 2 << 14, 2 << 16, 2 << 18, 2 << 20] {
-        let ef_vec = EliasFanoVec::<FastBitVector>::new(
-            &thread_rng()
-                .sample_iter(Standard)
-                .take(l)
-                .collect::<Vec<u64>>(),
-        );
+        let mut sequence = thread_rng()
+            .sample_iter(Standard)
+            .take(l)
+            .collect::<Vec<u64>>();
+        sequence.sort_unstable();
+        let ef_vec = EliasFanoVec::<FastBitVector>::new(&sequence);
 
         let sample = Uniform::new(ef_vec.get(0), u64::MAX);
 
