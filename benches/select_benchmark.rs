6d3340ed56fd98a5c65105237296e0ec430f6a6d
Johannes Hengstler <ubezl@student.kit.edu>
2023-05-29 00:17:17 +0200

    changed benchmark config for rank and select

--- benches/select_benchmark.rs
+++ benches/select_benchmark.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion, BenchmarkId};
 use rand::distributions::{Distribution, Uniform};
 use vers::RsVector;
 
@@ -7,11 +7,11 @@ mod common;
 fn bench_select(b: &mut Criterion) {
     let mut rng = rand::thread_rng();
 
-    let mut group = b.benchmark_group("select");
+    let mut group = b.benchmark_group("vers select");
     for l in [2 << 8, 2 << 10, 2 << 12, 2 << 14, 2 << 16, 2 << 18, 2 << 20] {
         let bit_vec = common::construct_vers_vec(&mut rng, l);
         let sample = Uniform::new(0, bit_vec.len() / 4);
-        group.bench_with_input(format!("{} elements", l), &l, |b, _| {
+        group.bench_with_input(BenchmarkId::new("select", l), &l, |b, _| {
             b.iter_batched(
                 || sample.sample(&mut rng),
                 |e| black_box(bit_vec.select0(e)),
