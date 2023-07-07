b35fa43ea8977eacf52503046484f8b1ce02efd3
Johannes Hengstler <ubezl@student.kit.edu>
2023-07-07 17:39:53 +0200

    implemented rmq mode for example

--- examples/ads_project.rs
+++ examples/ads_project.rs
@@ -5,6 +5,7 @@ use std::io::{BufRead, BufReader, Write};
 use std::time::Instant;
 use std::{env, io};
 use vers::{EliasFanoVec, FastBitVector};
+use vers::rmq::fast_rmq::FastRmq;
 
 fn main() {
     let mut args = env::args().skip(1);
@@ -42,13 +43,8 @@ fn handle_predecessor_benchmark(
     let mut line = String::new();
     input_reader.read_line(&mut line)?;
     let size = line.trim().parse::<usize>().unwrap();
-    let mut pred_buffer = Vec::with_capacity(size);
+    let pred_buffer = read_n_numbers(size, &mut input_reader)?;
     let mut req_buffer = Vec::new();
-    for _ in 0..size {
-        line.clear();
-        input_reader.read_line(&mut line)?;
-        pred_buffer.push(line.trim().parse::<u64>().unwrap());
-    }
 
     while input_reader.has_data_left()? {
         line.clear();
@@ -56,8 +52,8 @@ fn handle_predecessor_benchmark(
         req_buffer.push(line.trim().parse::<u64>().unwrap());
     }
 
-    let elias_fano_vec = EliasFanoVec::<FastBitVector>::new(&pred_buffer);
     let start = Instant::now();
+    let elias_fano_vec = EliasFanoVec::<FastBitVector>::new(&pred_buffer);
 
     for req in req_buffer.iter_mut() {
         *req = elias_fano_vec.pred(*req);
@@ -77,10 +73,58 @@ fn handle_predecessor_benchmark(
 }
 
 fn handle_rmq_benchmark(
-    _input_file: String,
-    _output_file: String,
+    input_file: String,
+    output_file: String,
 ) -> Result<(u128, usize), io::Error> {
-    unimplemented!()
+    let mut input_reader = BufReader::new(File::open(input_file)?);
+
+    let mut line = String::new();
+    input_reader.read_line(&mut line)?;
+    let size = line.trim().parse::<usize>().unwrap();
+
+    let rmq_buffer = read_n_numbers(size, &mut input_reader)?;
+    let mut req_buffer = Vec::new();
+
+    while input_reader.has_data_left()? {
+        line.clear();
+        input_reader.read_line(&mut line)?;
+        let mut range = line.split(',');
+        let begin = range.next().unwrap().trim().parse::<usize>().unwrap();
+        let end = range.next().unwrap().trim().parse::<usize>().unwrap();
+        req_buffer.push((begin, end));
+    }
+
+    let start = Instant::now();
+    let rmq = FastRmq::new(rmq_buffer);
+
+    for (begin, end) in req_buffer.iter_mut() {
+        *begin = rmq.range_min(*begin, *end);
+    }
+
+    let time = start.elapsed().as_millis();
+
+    let mut output_file = File::create(output_file)?;
+    let output = req_buffer
+        .into_iter()
+        .map(|(begin, _)| format!("{}", begin))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    output_file.write_all(output.as_bytes())?;
+
+    Ok((time, 8))
+}
+
+/// Read n lines and treat each as a 64 bit number.
+fn read_n_numbers(n: usize, reader: &mut BufReader<File>) -> Result<Vec<u64>, io::Error> {
+    let mut buffer = Vec::with_capacity(n);
+    let mut line = String::new();
+    for _ in 0..n {
+        line.clear();
+        reader.read_line(&mut line)?;
+        buffer.push(line.trim().parse::<u64>().unwrap());
+    }
+    Ok(buffer)
 }
 
 fn print_usage() -> ! {
