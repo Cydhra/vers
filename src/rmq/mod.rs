2f5de4d7bb4cbe102157622deb9ce9391d1ba54c
Johannes Hengstler <ubezl@student.kit.edu>
2023-08-02 23:17:42 +0200

    deleted unused trait

--- src/rmq/mod.rs
+++ src/rmq/mod.rs
@@ -5,17 +5,3 @@
 pub mod fast_rmq;
 
 pub mod binary_rmq;
-
-/// A common trait for range minimum query data structures to allow generic implementations.
-trait RangeMinimumVec {
-    fn new(data: &[u64]) -> Self;
-
-    /// Returns the index of the minimum element in the range [i, j].
-    fn range_min(&self, i: usize, j: usize) -> usize;
-
-    /// Returns the number of bytes used by the data structure.
-    fn heap_size(&self) -> usize;
-
-    /// Returns the number of elements in the vector.
-    fn len(&self) -> usize;
-}
