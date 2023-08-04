0a21b4b85fe3d2411bece6ea2fe575e127c0c918
Johannes Hengstler <ubezl@student.kit.edu>
2023-08-04 15:46:55 +0200

    added test case for illegal bitvec queries

--- src/bit_vec/tests.rs
+++ src/bit_vec/tests.rs
@@ -9,3 +9,24 @@ fn test_alloc_ones() {
     // test that unused bits are still zero, because this is what the data structure assumes
     assert_eq!(bv.data[0] >> 42, 0);
 }
+
+#[test]
+fn test_illegal_queries() {
+    let bv = BitVec::from_zeros(128);
+    assert_eq!(bv.len(), 128);
+
+    assert!(bv.get(128).is_none());
+    assert!(bv.get(129).is_none());
+
+    let result = std::panic::catch_unwind(|| {
+        let mut bv = BitVec::from_zeros(128);
+        bv.flip_bit(128)
+    });
+    assert!(result.is_err());
+
+    let result = std::panic::catch_unwind(|| {
+        let mut bv = BitVec::from_zeros(128);
+        bv.flip_bit(129)
+    });
+    assert!(result.is_err());
+}
