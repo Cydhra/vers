9482a4a36f456234037f2666128a323a38905c14
Johannes Hengstler <ubezl@student.kit.edu>
2023-08-04 16:06:33 +0200

    added test case for normal bitvec operations

--- src/bit_vec/tests.rs
+++ src/bit_vec/tests.rs
@@ -1,6 +1,30 @@
 use super::BitVec;
 
 #[test]
+fn simple_bit_vec_test() {
+    let mut bv = BitVec::default();
+
+    bv.append_word(2);
+    assert_eq!(bv.len(), 64);
+
+    bv.append_bit(1);
+    assert_eq!(bv.len(), 65);
+
+    assert_eq!(bv.get(0), Some(0));
+    assert_eq!(bv.get(1), Some(1));
+    (2..64).for_each(|i| assert_eq!(bv.get(i), Some(0), "mismatch at {}", i));
+    assert_eq!(bv.get(64), Some(1));
+
+    bv.flip_bit(1);
+    (0..64).for_each(|i| assert_eq!(bv.get(i), Some(0), "mismatch after flip at {}", i));
+
+    bv.truncate(64);
+    assert_eq!(bv.len(), 1);
+    assert_eq!(bv.get(0), Some(0));
+    assert_eq!(bv.get(1), None);
+}
+
+#[test]
 fn test_alloc_ones() {
     let bv = BitVec::from_ones(42);
     assert_eq!(bv.len(), 42);
