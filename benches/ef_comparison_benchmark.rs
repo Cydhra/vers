94670d511a227dd1672335742f2a67c7d89307dc
Johannes Hengstler <ubezl@student.kit.edu>
2023-08-05 23:46:31 +0200

    added in-order access benchmark for elias fano

--- benches/ef_comparison_benchmark.rs
+++ benches/ef_comparison_benchmark.rs
@@ -2,6 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, BatchSize, Benchmark
 use elias_fano::EliasFano;
 use rand::distributions::{Distribution, Standard, Uniform};
 use rand::{thread_rng, Rng};
+use std::time::{Duration, Instant};
 use vers_vecs::EliasFanoVec;
 
 mod common;
@@ -40,6 +41,60 @@ fn bench_ef(b: &mut Criterion) {
                 |e| black_box(comparison_ef_vec.visit(e as u64)),
                 BatchSize::SmallInput,
             )
+        });
+    }
+    group.finish();
+
+    let mut group = b.benchmark_group("Elias-Fano Comparison: in-order-access");
+    group.plot_config(common::plot_config());
+
+    for l in common::SIZES {
+        let mut sequence = thread_rng()
+            .sample_iter(Standard)
+            .take(l)
+            .collect::<Vec<u64>>();
+        sequence.sort_unstable();
+
+        let ef_vec = EliasFanoVec::from_slice(&sequence);
+        let mut comparison_ef_vec =
+            EliasFano::new(sequence[sequence.len() - 1], sequence.len() as u64);
+        comparison_ef_vec.compress(sequence.iter());
+
+        group.bench_with_input(BenchmarkId::new("vers vector", l), &l, |b, _| {
+            b.iter_custom(|iters| {
+                let mut time = Duration::new(0, 0);
+                let mut i = 0;
+
+                while i < iters {
+                    let iter = ef_vec.iter().take((iters - i) as usize);
+                    let start = Instant::now();
+                    for e in iter {
+                        black_box(e);
+                        i += 1;
+                    }
+                    time += start.elapsed();
+                }
+
+                time
+            })
+        });
+
+        group.bench_with_input(BenchmarkId::new("elias fano vector", l), &l, |b, _| {
+            b.iter_custom(|iters| {
+                let mut time = Duration::new(0, 0);
+                let mut i = 0;
+
+                while i < iters {
+                    comparison_ef_vec.reset();
+                    let start = Instant::now();
+                    while comparison_ef_vec.next().is_ok() && i < iters {
+                        i += 1;
+                    }
+                    time += start.elapsed();
+                }
+
+                time
+            })
         });
     }
     group.finish();
