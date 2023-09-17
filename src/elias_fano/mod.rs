a33f4d6a921975be59db25fe14def50d9523c7a6
chris-ha458 <hac541309@gmail.com>
2023-09-17 21:48:37 +0900

    last default clippy

--- src/elias_fano/mod.rs
+++ src/elias_fano/mod.rs
@@ -76,7 +76,7 @@ impl EliasFanoVec {
     #[must_use]
     #[allow(clippy::cast_possible_truncation)]
     pub fn from_slice(data: &[u64]) -> Self {
-        if data.len() == 0 {
+        if data.is_empty() {
             return Self {
                 upper_vec: RsVec::from_bit_vec(BitVec::new()),
                 lower_vec: BitVec::new(),
