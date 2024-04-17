990e46bb95fdb04728c7fdb3418435cb9e46e085
Johannes Hengstler <ubezl@student.kit.edu>
2024-04-17 12:22:54 +0200

    removed comparison benchmarks to move them into a separate crate/repository

--- benches/worst_select_benchmark.rs
+++ benches/worst_select_benchmark.rs
@@ -1,15 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use rand::thread_rng;
-use rsdict::RsDict;
-// use sucds::bit_vectors::darray::DArray as SucDArray;
-// use sucds::bit_vectors::{BitVector as SucBitVec, Select};
 use vers_vecs::{BitVec, RsVec};
 
 mod common;
 
-fn bench_ef(b: &mut Criterion) {
+fn select_worst_case(b: &mut Criterion) {
     let mut rng = thread_rng();
-    let mut group = b.benchmark_group("Select Benchmark: Worst Case Input");
+    let mut group = b.benchmark_group("Select: Adversarial Input");
     group.plot_config(common::plot_config());
 
     for order_of_magnitude in [14, 16, 18, 20, 22, 24, 26] {
@@ -44,55 +41,9 @@ fn bench_ef(b: &mut Criterion) {
             |b, _| b.iter(|| black_box(bit_vec.select1((1 << 13) - 1))),
         );
         drop(bit_vec);
-
-        let mut rs_dict = RsDict::with_capacity(length / 64);
-        for _ in 0..(1usize << 13) - 1 {
-            rs_dict.push(true);
-        }
-        rs_dict.push(false);
-
-        for _ in 0..(length - (1 << 13)) - 1 {
-            rs_dict.push(false);
-        }
-        rs_dict.push(true);
-        rs_dict.push(true);
-        rs_dict.push(false);
-        rs_dict.push(false);
-
-        group.bench_with_input(
-            BenchmarkId::new("rsdict worst case input", length),
-            &length,
-            |b, _| b.iter(|| black_box(rs_dict.select1((1 << 13) - 1))),
-        );
-        drop(rs_dict);
-
-        // the following benchmark does not trigger the worst-case for sucd reliably,
-        // because it utilizes compressed arrays.
-        // TODO: find out how to trigger a worst-case for sucd (if there is one)
-
-        // let mut suc_bv = SucBitVec::with_capacity(length / 64);
-        // for _ in 0..(1usize << 13) / 64 - 1 {
-        //     suc_bv
-        //         .push_bits(u64::MAX as usize, 64)
-        //         .expect("push_bits failed");
-        // }
-        // suc_bv
-        //     .push_bits((u64::MAX >> 1) as usize, 64)
-        //     .expect("push_bits failed");
-        //
-        // for _ in 0..(length - (1 << 13)) / 64 - 1 {
-        //     suc_bv.push_bits(0, 64).expect("push_bits failed");
-        // }
-        // suc_bv.push_bits(2, 64).expect("push_bits failed");
-        //
-        // let sucds_darray = SucDArray::from_bits(suc_bv.iter()).enable_rank();
-        // group.bench_with_input(BenchmarkId::new("sucds worst case input", length), &length, |b, _| {
-        //     b.iter(|| black_box(sucds_darray.select1((1 << 13) - 1)))
-        // });
-        // drop(sucds_darray);
     }
     group.finish();
 }
 
-criterion_group!(benches, bench_ef);
+criterion_group!(benches, select_worst_case);
 criterion_main!(benches);
