16aeebb757176a915a3d14d2c2797bc6ab0d7521
Johannes Hengstler <ubezl@student.kit.edu>
2024-09-08 18:39:41 +0200

    untested: excess tree construction

--- src/trees/mod.rs
+++ src/trees/mod.rs
@@ -1,9 +1,11 @@
+use crate::BitVec;
 use std::num::NonZeroUsize;
 use std::ops::Index;
 
 mod bp;
 
 /// A singular node in a binary min-max tree that is part of the BpTree data structure.
+#[derive(Debug, Clone, Default)]
 struct MinMaxNode {
     /// excess from 0..=r in the node [l, r]
     total_excess: usize,
@@ -21,6 +23,59 @@ struct MinMaxTree {
 }
 
 impl MinMaxTree {
+    fn excess_tree(bit_vec: &BitVec, block_size: usize) -> Self {
+        let num_leaves = (bit_vec.len() + block_size - 1) / block_size;
+        let mut num_internal_nodes = (num_leaves as f64).log2().ceil() as usize;
+
+        let mut nodes = vec![MinMaxNode::default(); num_leaves + num_internal_nodes];
+        let mut total_excess = 0;
+        let mut min_excess = 0;
+        let mut max_excess = 0;
+
+        // bottom up construction
+        for i in 0..bit_vec.len() {
+            if i > 0 && i % block_size == 0 {
+                nodes[num_internal_nodes + i / block_size - 1] = MinMaxNode {
+                    total_excess,
+                    min_excess,
+                    max_excess,
+                };
+                min_excess = total_excess;
+                max_excess = total_excess;
+            }
+            total_excess += if bit_vec[i] { 1 } else { -1 };
+            min_excess = min_excess.min(total_excess);
+            max_excess = max_excess.max(total_excess);
+        }
+        nodes[num_internal_nodes + num_leaves - 1] = MinMaxNode {
+            total_excess,
+            min_excess,
+            max_excess,
+        };
+
+        let mut current_level_size = num_leaves.next_power_of_two() / 2;
+        let mut current_level_start = num_internal_nodes - 1 - current_level_size;
+        while current_level_start > 0 {
+            for i in 0..current_level_size {
+                let left_child = &nodes[(current_level_start + i) * 2 + 1];
+                let right_child = &nodes[(current_level_start + i) * 2 + 2];
+                nodes[current_level_start + i] = MinMaxNode {
+                    total_excess: left_child.total_excess,
+                    min_excess: left_child.min_excess.min(
+                        left_child.total_excess + right_child.min_excess - right_child.total_excess,
+                    ),
+                    max_excess: left_child.max_excess.max(
+                        left_child.total_excess + right_child.max_excess - right_child.total_excess,
+                    ),
+                };
+            }
+            current_level_size /= 2;
+            current_level_start -= current_level_size;
+        }
+
+        Self { nodes }
+    }
+
     fn total_excess(&self, index: usize) -> usize {
         self.nodes[index].total_excess
     }
