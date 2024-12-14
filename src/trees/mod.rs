0c81a56d6c1a6cc472848ca5f663f7e6cb68a6c4
Johannes Hengstler <ubezl@student.kit.edu>
2024-12-14 16:18:17 +0100

    parent function returns None if necessary, implemented test for minmax tree navigation

--- src/trees/mod.rs
+++ src/trees/mod.rs
@@ -18,12 +18,12 @@ struct MinMaxNode {
 
 /// A binary min-max tree that is part of the BpTree data structure.
 #[derive(Clone, Debug, Default)]
-struct MinMaxTree {
+pub struct MinMaxTree {
     nodes: Vec<MinMaxNode>,
 }
 
 impl MinMaxTree {
-    fn excess_tree(bit_vec: &BitVec, block_size: usize) -> Self {
+    pub fn excess_tree(bit_vec: &BitVec, block_size: usize) -> Self {
         if bit_vec.is_empty() {
             return Self::default();
         }
@@ -100,28 +100,28 @@ impl MinMaxTree {
         Self { nodes }
     }
 
-    fn total_excess(&self, index: usize) -> isize {
+    pub fn total_excess(&self, index: usize) -> isize {
         self.nodes[index].total_excess
     }
 
-    fn min_excess(&self, index: usize) -> isize {
+    pub fn min_excess(&self, index: usize) -> isize {
         self.nodes[index].min_excess
     }
 
-    fn max_excess(&self, index: usize) -> isize {
+    pub fn max_excess(&self, index: usize) -> isize {
         self.nodes[index].max_excess
     }
 
-    fn parent(&self, index: NonZeroUsize) -> usize {
-        debug_assert!(
-            index.get() < self.nodes.len(),
-            "request parent for non-existent node"
-        );
-        (index.get() - 1) / 2
+    pub fn parent(&self, index: NonZeroUsize) -> Option<usize> {
+        if index.get() < self.nodes.len() {
+            Some((index.get() - 1) / 2)
+        } else {
+            None
+        }
     }
 
     /// Get the index of the left child of the node at `index` if it exists
-    fn left_child(&self, index: usize) -> Option<NonZeroUsize> {
+    pub fn left_child(&self, index: usize) -> Option<NonZeroUsize> {
         if index * 2 + 1 < self.nodes.len() {
             NonZeroUsize::new(index * 2 + 1)
         } else {
@@ -130,7 +130,7 @@ impl MinMaxTree {
     }
 
     /// Get the index of the right child of the node at `index` if it exists
-    fn right_child(&self, index: usize) -> Option<NonZeroUsize> {
+    pub fn right_child(&self, index: usize) -> Option<NonZeroUsize> {
         if index * 2 + 2 < self.nodes.len() {
             NonZeroUsize::new(index * 2 + 2)
         } else {
@@ -139,7 +139,7 @@ impl MinMaxTree {
     }
 
     /// Get the index of the right sibling of the node at `index` if it exists
-    fn right_sibling(&self, index: NonZeroUsize) -> Option<NonZeroUsize> {
+    pub fn right_sibling(&self, index: NonZeroUsize) -> Option<NonZeroUsize> {
         if index.get() % 2 == 1 {
             if index.get() + 1 >= self.nodes.len() {
                 None
@@ -152,12 +152,21 @@ impl MinMaxTree {
     }
 
     /// Get the index of the left sibling of the node at `index` if it exists
-    fn left_sibling(&self, index: NonZeroUsize) -> Option<NonZeroUsize> {
+    pub fn left_sibling(&self, index: NonZeroUsize) -> Option<NonZeroUsize> {
         if index.get() % 2 == 0 {
             // index is at least 2
             NonZeroUsize::new(index.get() - 1)
         } else {
             None
+        }
+    }
+
+    /// Get the index of the root node if it exists
+    pub fn root(&self) -> Option<usize> {
+        if self.nodes.is_empty() {
+            None
+        } else {
+            Some(0)
         }
     }
 }
@@ -216,5 +225,80 @@ mod tests {
         let tree = MinMaxTree::excess_tree(&bv, 8);
 
         assert_eq!(tree.nodes.len(), 0);
+    }
+
+    #[test]
+    fn test_excess_tree_navigation() {
+        // expected tree layout:
+        //      0
+        //    /  \
+        //   1    2
+        //   /\  /\
+        //  3  4 5 6
+        //  /\/\/\/\
+        // 7 8 9 10 11 12 - -
+        let bv = BitVec::from_bits(&vec![0; 48]);
+        let tree = MinMaxTree::excess_tree(&bv, 8);
+
+        assert_eq!(tree.nodes.len(), 13); // 6 leaves + 7 internal nodes
+
+        // check root
+        assert_eq!(tree.root(), Some(0));
+        assert_eq!(tree.left_child(0), NonZeroUsize::new(1));
+        assert_eq!(tree.right_child(0), NonZeroUsize::new(2));
+
+        // check full nodes
+        for node in 1..=5 {
+            assert_eq!(tree.left_child(node), NonZeroUsize::new(node * 2 + 1));
+            assert_eq!(tree.right_child(node), NonZeroUsize::new(node * 2 + 2));
+        }
+
+        // check obsolete node
+        assert_eq!(tree.left_child(6), None);
+        assert_eq!(tree.right_child(6), None);
+
+        // check siblings of first level
+        assert_eq!(tree.left_sibling(NonZeroUsize::new(1).unwrap()), None);
+        assert_eq!(
+            tree.right_sibling(NonZeroUsize::new(1).unwrap()),
+            NonZeroUsize::new(2)
+        );
+
+        assert_eq!(
+            tree.left_sibling(NonZeroUsize::new(2).unwrap()),
+            NonZeroUsize::new(1)
+        );
+        assert_eq!(tree.right_sibling(NonZeroUsize::new(2).unwrap()), None);
+
+        // check siblings of leaf nodes
+        assert_eq!(tree.left_sibling(NonZeroUsize::new(7).unwrap()), None);
+        assert_eq!(
+            tree.right_sibling(NonZeroUsize::new(7).unwrap()),
+            NonZeroUsize::new(8)
+        );
+
+        // leaves are not connected to each other because we don't need it for the search primitives
+        assert_eq!(
+            tree.left_sibling(NonZeroUsize::new(8).unwrap()),
+            NonZeroUsize::new(7)
+        );
+        assert_eq!(tree.right_sibling(NonZeroUsize::new(8).unwrap()), None);
+
+        // check siblings of non-existent node
+        assert_eq!(tree.left_sibling(NonZeroUsize::new(13).unwrap()), None);
+        assert_eq!(tree.right_sibling(NonZeroUsize::new(13).unwrap()), None);
+
+        // check parent of leaf nodes
+        assert_eq!(tree.parent(NonZeroUsize::new(7).unwrap()), Some(3));
+        assert_eq!(tree.parent(NonZeroUsize::new(8).unwrap()), Some(3));
+        assert_eq!(tree.parent(NonZeroUsize::new(9).unwrap()), Some(4));
+        assert_eq!(tree.parent(NonZeroUsize::new(10).unwrap()), Some(4));
+
+        // check parent of first level nodes
+        assert_eq!(tree.parent(NonZeroUsize::new(1).unwrap()), Some(0));
+        assert_eq!(tree.parent(NonZeroUsize::new(2).unwrap()), Some(0));
+
+        // check parent of non-existent node
+        assert_eq!(tree.parent(NonZeroUsize::new(13).unwrap()), None);
     }
 }
